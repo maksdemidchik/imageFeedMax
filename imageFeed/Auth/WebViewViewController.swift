a21bc063d12da946d10068f2804123cfe38ecede
Maksim Demidchik <maksimdemidcik37@gmail.com>
2025-03-05 02:32:29 +0300

    mod1

--- imageFeed/Auth/WebViewViewController.swift
+++ imageFeed/Auth/WebViewViewController.swift
@@ -42,8 +42,7 @@ final class WebViewViewController: UIViewController {
         let backButton = UIButton.systemButton(with: UIImage(named: "nav_back_button_white") ?? UIImage(), target: self, action: #selector(self.backButtonAction))
         buttonSettings(button: backButton)
         self.backButton = backButton
-        progressView.topAnchor.constraint(equalTo: backButton.topAnchor,constant: 8).isActive = true
-        webView.topAnchor.constraint(equalTo: backButton.topAnchor,constant: 8).isActive = true
+       
     }
     
     override func viewDidAppear(_ animated: Bool) {
